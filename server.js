0de4ba2648020085d47eb2189608a06f89fadceb
rexthelinebarre <rexthelinebarrel@163.com>
2026-05-23 19:37:06 +0800

    refactor: extract Store to lib/store.js

--- server.js
+++ server.js
@@ -9,74 +9,7 @@ const { HighlightStore } = require('./lib/highlight-store');
 const { analyzeAudio } = require('./lib/audio-analyzer');
 
 const logger = require('./lib/logger');
-
-const CONFIG_PATH = path.join(__dirname, 'config.json');
-const DEFAULT_CONFIG = {
-  streamers: [],
-  settings: { savePath: path.join(__dirname, 'recordings'), format: 'flv' }
-};
-
-const Store = {
-  _data: null,
-
-  load() {
-    try {
-      const raw = fs.readFileSync(CONFIG_PATH, 'utf-8');
-      this._data = JSON.parse(raw);
-    } catch {
-      this._data = JSON.parse(JSON.stringify(DEFAULT_CONFIG));
-      this.save();
-    }
-    return this._data;
-  },
-
-  save() {
-    fs.writeFileSync(CONFIG_PATH, JSON.stringify(this._data, null, 2), 'utf-8');
-  },
-
-  getAll() { return this._data; },
-
-  getStreamers() { return this._data.streamers; },
-
-  addStreamer(s) {
-    this._data.streamers.push(s);
-    this.save();
-    return s;
-  },
-
-  removeStreamer(id) {
-    this._data.streamers = this._data.streamers.filter(s => s.id !== id);
-    this.save();
-  },
-
-  updateStreamer(id, updates) {
-    const idx = this._data.streamers.findIndex(s => s.id === id);
-    if (idx !== -1) {
-      Object.assign(this._data.streamers[idx], updates);
-      this.save();
-    }
-  },
-
-  getSettings() { return this._data.settings; },
-
-  updateSettings(updates) {
-    Object.assign(this._data.settings, updates);
-    this.save();
-  }
-};
-
-Store.load();
-
-// Ensure emotionDict exists
-if (!Store._data.emotionDict) {
-  Store._data.emotionDict = {
-    laugh: ["哈哈", "笑死", "草", "www", "hhh", "笑死我了"],
-    surprise: ["？？", "卧槽", "啊？", "什么", "我去", "wc"],
-    praise: ["666", "牛逼", "太强了", "牛", "帅", "好强"],
-    mock: ["离谱", "逆天", "不愧是你", "就这", "典", "急"]
-  };
-  Store.save();
-}
+const Store = require('./lib/store');
 
 const BiliAPI = {
   _get(url) {
